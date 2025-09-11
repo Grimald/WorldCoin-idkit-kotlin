f32006a1581a18b24242018eaecf4cf1e11d89f6
Manu Singh <manu.singh@toolsforhumanity.com>
2025-09-12 08:43:51 +0900

    [APP-9080] [APP] Add Publishing functionality to idkit-kotlin (#16)

--- idkit-kotlin/build.gradle.kts
+++ idkit-kotlin/build.gradle.kts
@@ -25,10 +25,41 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
     implementation(libs.material)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlincrypto.sha3)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.worldcoin"
+                artifactId = "idkit-kotlin"
+                // Update this version when publishing
+                version = "2.0.1"
+                from(components["release"])
+            }
+        }
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/worldcoin/idkit-kotlin")
+                credentials {
+                    username = System.getenv("GITHUB_USER")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
 }
